085ef3772b22728278497a005ecb810226b34894
pihero <kitakitsune1110@gmail.com>
2021-12-28 16:53:37 +0900

    Localize main storyboard

--- BaseInternationalization/ViewController.swift
+++ BaseInternationalization/ViewController.swift
@@ -1,19 +1,11 @@
-//
-//  ViewController.swift
-//  BaseInternationalization
-//
-//  Created by 廣川 昂紀 on 2021/12/28.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var greetingLabel: UILabel! {
+        didSet {
+            greetingLabel.text = NSLocalizedString("Hello", comment: "")
+        }
     }
-
-
 }
 
